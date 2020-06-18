d83531c5a2234df49d9cf07901909181731ded20
jarvis <jarvisrizo_southsystem@getnet.br>
2020-06-17 22:44:24 -0300

    adding vets index and controller - Closes #13

---
+++ pet-cinic-web/src/main/java/guru/petclinic/petclinic/controllers/OwnerController.java
@@ -0,0 +1,17 @@
+package guru.petclinic.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+
+    @GetMapping()
+    public String listOwner(){
+
+        return "owners/index";
+    }
+
+}
