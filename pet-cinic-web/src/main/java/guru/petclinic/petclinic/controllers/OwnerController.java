e477a635ff9c8aee2659dcbc4c7936434d6336b4
jarvis <jarvisrizo_southsystem@getnet.br>
2020-06-24 21:21:19 -0300

    Listing Owners - Closes #16

--- pet-cinic-web/src/main/java/guru/petclinic/petclinic/controllers/OwnerController.java
+++ pet-cinic-web/src/main/java/guru/petclinic/petclinic/controllers/OwnerController.java
@@ -1,15 +1,29 @@
 package guru.petclinic.petclinic.controllers;
 
+import guru.petclinic.petclinic.model.Owner;
+import guru.petclinic.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @GetMapping()
-    public String listOwner(){
+    public String listOwner(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
     }
--- pet-clinic-data/src/main/java/guru/petclinic/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/petclinic/petclinic/services/OwnerService.java
@@ -2,8 +2,14 @@ package guru.petclinic.petclinic.services;
 
 import guru.petclinic.petclinic.model.Owner;
 
+import java.util.Set;
+
 public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
 
+    @Override
+    default Set<Owner> findAll() {
+        return null;
+    }
 }
