78d1466588615a805f583f45edaeb6af4f753c5d
jarvis <jarvisrizo_southsystem@getnet.br>
2020-07-02 22:11:22 -0300

    Add Pet Type Servis Map - Closes #45

---
+++ pet-clinic-data/src/main/java/guru/petclinic/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.petclinic.petclinic.services;
+
+import guru.petclinic.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
---
+++ pet-clinic-data/src/main/java/guru/petclinic/petclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,34 @@
+package guru.petclinic.petclinic.services.map;
+
+import guru.petclinic.petclinic.model.PetType;
+import guru.petclinic.petclinic.services.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
